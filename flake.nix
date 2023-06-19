5a294e65b8490f60e1c417bb5748ff349d5c57b9
256randombits <121695324+256randombits@users.noreply.github.com>
2023-06-19 19:53:31 +0300

    Add template

--- flake.nix
+++ flake.nix
@@ -6,168 +6,175 @@
       flake-utils.url = "github:numtide/flake-utils";
     };
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        # texlive = pkgs.texlive.combined.scheme-full;
-        texlive = pkgs.texlive.combined.scheme-small;
-        docker = {
-          image = {
-            name = "latex-tools";
-            tag = "dev";
-          };
-        };
-        set-environment = pkgs.writeShellScript "set-env.sh" ''
-          export PROJECTPATH="''${PROJECTPATH:-$(${pkgs.git}/bin/git rev-parse --show-toplevel)}"
-
-          # shellcheck source=/dev/null
-          source "''${PROJECTPATH}"/settings.env
-        '';
-      in
-      {
-        packages = {
-
-          document-compiler = pkgs.writeShellApplication {
-            name = "document-compiler";
-            runtimeInputs = [ texlive pkgs.coreutils ];
-            text = ''
-              # shellcheck source=/dev/null
-              source ${set-environment}
-
-              pushd "''${PROJECTPATH}"
-              mkdir -p "''${OUTDIR}"
-              popd
-
-              pushd "''${PROJECTPATH}"/"''${SRCDIR}"
-
-              pdflatex \
-                -output-directory="''${PROJECTPATH}"/"''${OUTDIR}" \
-                -jobname="''${JOBNAME}" \
-                "''${TEXFILE}"
-
-
-              popd
-            '';
-          };
-
-          file-watcher = pkgs.writeShellApplication {
-            name = "file-watcher";
-            runtimeInputs = [
-              self.packages.${system}.document-compiler
-              pkgs.findutils
-              pkgs.entr
-            ];
-            text = ''
-              # shellcheck source=/dev/null
-              source ${set-environment}
-              pushd "''${PROJECTPATH}"/"''${SRCDIR}"
-
-              find . -name "*.tex" | entr document-compiler
-
-              popd
-            '';
-          };
-
-          docker-image = pkgs.dockerTools.buildLayeredImage {
-            name = docker.image.name;
-            tag = docker.image.tag;
-            contents = [
-              pkgs.bash
-              self.packages.${system}.file-watcher
-            ];
-            config = {
-              Cmd = [ "file-watcher" ];
-              WorkingDir = "/data";
-              Volumes = {
-                "/data" = { };
-              };
-              Env = [
-                "PROJECTPATH=/data"
-              ];
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          # texlive = pkgs.texlive.combined.scheme-full;
+          texlive = pkgs.texlive.combined.scheme-small;
+          docker = {
+            image = {
+              name = "latex-tools";
+              tag = "dev";
             };
           };
-        };
-        apps = {
-          default = self.apps.${system}.document-compile;
-          file-watch = flake-utils.lib.mkApp { drv = self.packages.${system}.file-watcher; };
-          document-compile = flake-utils.lib.mkApp { drv = self.packages.${system}.document-compiler; };
-          all = flake-utils.lib.mkApp {
-            drv = pkgs.writeShellApplication {
-              name = "document-all";
-              runtimeInputs = [
-                self.packages.${system}.document-compiler
-                self.packages.${system}.file-watcher
-                pkgs.zathura
-              ];
+          set-environment = pkgs.writeShellScript "set-env.sh" ''
+            export PROJECTPATH="''${PROJECTPATH:-$(${pkgs.git}/bin/git rev-parse --show-toplevel)}"
+
+            # shellcheck source=/dev/null
+            source "''${PROJECTPATH}"/settings.env
+          '';
+        in
+        {
+          packages = {
+
+            document-compiler = pkgs.writeShellApplication {
+              name = "document-compiler";
+              runtimeInputs = [ texlive pkgs.coreutils ];
               text = ''
                 # shellcheck source=/dev/null
                 source ${set-environment}
+
                 pushd "''${PROJECTPATH}"
+                mkdir -p "''${OUTDIR}"
+                popd
 
-                #Run compiler once to make sure that the pdf exists.
-                document-compiler
+                pushd "''${PROJECTPATH}"/"''${SRCDIR}"
 
-                file-watcher &
-                # file_watcher_pid="$!"
+                pdflatex \
+                  -output-directory="''${PROJECTPATH}"/"''${OUTDIR}" \
+                  -jobname="''${JOBNAME}" \
+                  "''${TEXFILE}"
 
-                zathura "''${PROJECTPATH}"/"''${OUTDIR}"/"''${JOBNAME}".pdf &
-                # zathura_pid=$!
-
-                trap 'pkill -P $$' SIGINT
-
-                wait
 
                 popd
               '';
             };
-          };
-          docker-import = flake-utils.lib.mkApp {
-            drv = pkgs.writeShellApplication {
-              name = "docker-import";
-              runtimeInputs = [
-                pkgs.which
-              ];
-              text = ''
-                if ! which "docker" > /dev/null 2>&1
-                then
-                  echo "Docker not found in PATH!"
-                  exit 1
-                fi
 
-                docker load < ${self.packages.${system}.docker-image}
-              '';
-            };
-          };
-          docker-run = flake-utils.lib.mkApp {
-            drv = pkgs.writeShellApplication {
-              name = "docker-run";
+            file-watcher = pkgs.writeShellApplication {
+              name = "file-watcher";
+              runtimeInputs = [
+                self.packages.${system}.document-compiler
+                pkgs.findutils
+                pkgs.entr
+              ];
               text = ''
                 # shellcheck source=/dev/null
                 source ${set-environment}
+                pushd "''${PROJECTPATH}"/"''${SRCDIR}"
 
-                ${self.apps.${system}.docker-import.program}
-                docker run -it -v "''${PROJECTPATH}":/data ${docker.image.name}:${docker.image.tag}
+                find . -name "*.tex" | entr document-compiler
+
+                popd
               '';
             };
-          };
-        };
-        devShells = {
-          default = pkgs.mkShell {
-            nativeBuildInputs =
-              (with pkgs; [
-                zathura
-                entr
-              ]) ++ [
-                texlive
-              ];
-          };
-          editReadme = pkgs.mkShell {
-            nativeBuildInputs = with pkgs;
-              [
-                python311Packages.grip
-              ];
-          };
 
-        };
-      });
+            docker-image = pkgs.dockerTools.buildLayeredImage {
+              name = docker.image.name;
+              tag = docker.image.tag;
+              contents = [
+                pkgs.bash
+                self.packages.${system}.file-watcher
+              ];
+              config = {
+                Cmd = [ "file-watcher" ];
+                WorkingDir = "/data";
+                Volumes = {
+                  "/data" = { };
+                };
+                Env = [
+                  "PROJECTPATH=/data"
+                ];
+              };
+            };
+          };
+          apps = {
+            default = self.apps.${system}.document-compile;
+            file-watch = flake-utils.lib.mkApp { drv = self.packages.${system}.file-watcher; };
+            document-compile = flake-utils.lib.mkApp { drv = self.packages.${system}.document-compiler; };
+            all = flake-utils.lib.mkApp {
+              drv = pkgs.writeShellApplication {
+                name = "document-all";
+                runtimeInputs = [
+                  self.packages.${system}.document-compiler
+                  self.packages.${system}.file-watcher
+                  pkgs.zathura
+                ];
+                text = ''
+                  # shellcheck source=/dev/null
+                  source ${set-environment}
+                  pushd "''${PROJECTPATH}"
+
+                  #Run compiler once to make sure that the pdf exists.
+                  document-compiler
+
+                  file-watcher &
+                  # file_watcher_pid="$!"
+
+                  zathura "''${PROJECTPATH}"/"''${OUTDIR}"/"''${JOBNAME}".pdf &
+                  # zathura_pid=$!
+
+                  trap 'pkill -P $$' SIGINT
+
+                  wait
+
+                  popd
+                '';
+              };
+            };
+            docker-import = flake-utils.lib.mkApp {
+              drv = pkgs.writeShellApplication {
+                name = "docker-import";
+                runtimeInputs = [
+                  pkgs.which
+                ];
+                text = ''
+                  if ! which "docker" > /dev/null 2>&1
+                  then
+                    echo "Docker not found in PATH!"
+                    exit 1
+                  fi
+
+                  docker load < ${self.packages.${system}.docker-image}
+                '';
+              };
+            };
+            docker-run = flake-utils.lib.mkApp {
+              drv = pkgs.writeShellApplication {
+                name = "docker-run";
+                text = ''
+                  # shellcheck source=/dev/null
+                  source ${set-environment}
+
+                  ${self.apps.${system}.docker-import.program}
+                  docker run -it -v "''${PROJECTPATH}":/data ${docker.image.name}:${docker.image.tag}
+                '';
+              };
+            };
+          };
+          devShells = {
+            default = pkgs.mkShell {
+              nativeBuildInputs =
+                (with pkgs; [
+                  zathura
+                  entr
+                ]) ++ [
+                  texlive
+                ];
+            };
+            editReadme = pkgs.mkShell {
+              nativeBuildInputs = with pkgs;
+                [
+                  python311Packages.grip
+                ];
+            };
+
+          };
+        }) //
+    {
+      templates.default = {
+        path = ./template;
+        description = "A LaTeX document";
+      };
+    };
 }
---
+++ template/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A LaTeX Document";
+  inputs =
+    {
+      latex-flake.url = "github:256randombits/latex-flake";
+      flake-utils.url = "github:numtide/flake-utils";
+    };
+  outputs = { latex-flake, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        {
+          apps = {
+            file-watch = latex-flake.apps.${system}.file-watch;
+          };
+        });
+}
